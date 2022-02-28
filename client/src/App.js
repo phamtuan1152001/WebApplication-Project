cfca6986f0625a8d5b0ef67adb1f1a2ec89cb739
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2022-02-28 19:22:04 +0700

    Doing task Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/pages/Home";
+import ProductsPage from "./components/pages/Products";
+import ServicesPage from "./components/pages/Services";
+import ContactPage from "./components/pages/Contact";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>song tuan</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <div>
+            <Link to="/" className="navbar-logo">
+              <HiOutlineShoppingBag className="navbar-icon" />
+              TNTTeam
+            </Link>
+          </div>
+          <div className="nav">
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/services" className="nav-links">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/products" className="nav-links">
+                  Products
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-links">
+                  Contact
+                </Link>
+              </li>
+              {/* <li className="nav-btn">
+              {button ? (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline">Sign Up</Button>
+                </Link>
+              ) : (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                    Sign Up
+                  </Button>
+                </Link>
+              )}
+            </li> */}
+            </ul>
+          </div>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
