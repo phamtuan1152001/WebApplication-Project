72ecc029193d2f701c5439b487743c6c398ab608
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2022-03-09 00:54:03 +0700

    Continue task Products ( 70% finishing )

--- client/src/components/pages/Products/ProductList/ProductList.js
+++ client/src/components/pages/Products/ProductList/ProductList.js
@@ -1,17 +1,88 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import "./ProductList.css";
 
-function ProductList({ products }) {
-  return (
-    <>
-      <div className="products-list-container">
-        {products.map((product) => (
-          <div key={product.id} className="products-list-item">
-            <img src={product.img} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
+function ProductList(
+  {
+    /* products */
+    /* img,
+  name,
+  price, */
+  }
+) {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  let componentMounted = true;
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+        console.log(filter);
+      }
+      return () => {
+        componentMounted = false;
+      };
+    };
+
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return (
+      <>
+        <div>
+          <Skeleton height={350} />
+        </div>
+        <div>
+          <Skeleton height={350} />
+        </div>
+        <div>
+          <Skeleton height={350} />
+        </div>
+        <div>
+          <Skeleton height={350} />
+        </div>
+      </>
+    );
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        {filter.map((product) => (
+          <div className="products-list-item card-products" key={product.id}>
+            <img
+              className="img-products"
+              src={product.image}
+              alt={product.title}
+            />
+            <h3>{product.title.substring(0, 12)}</h3>
+            <p>
+              <span>$</span>
+              {product.price}
+            </p>
           </div>
         ))}
-      </div>
+      </>
+    );
+  };
+
+  return (
+    <>
+      {/* <div className="products-list-item">
+        {<img src={img} alt={name} />
+        <h3>{name}</h3>
+        <p>{price}</p>}
+        {loading ? <Loading /> : <ShowProducts />}
+      </div> */}
+      {loading ? <Loading /> : <ShowProducts />}
     </>
   );
 }
--- client/src/components/pages/Products/Products.js
+++ client/src/components/pages/Products/Products.js
@@ -1,15 +1,27 @@
 import React from "react";
 import ProductList from "./ProductList/ProductList";
 import ProductSelection from "./ProductSelection/ProductSelection";
-//import { Grid } from "@material-ui/core";
-import products from "./dataProducts";
+import "./Products.css";
+//import products from "./dataProducts";
 
 function Products() {
   return (
     <>
       <div className="products-container">
-        <ProductSelection />
-        <ProductList products={products} />
+        <div className="products-selections-container">
+          <ProductSelection />
+        </div>
+        <div className="products-list-container">
+          {/* {products.map((product) => (
+            <ProductList
+              key={product.id}
+              img={product.img}
+              name={product.name}
+              price={product.price}
+            />
+          ))} */}
+          <ProductList />
+        </div>
       </div>
     </>
   );
