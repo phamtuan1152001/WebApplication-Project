398f2e47d8abd34b64bf7d715c87e3991ed9555f
gianamnguyen <gianamnguyen023@gmail.com>
2022-03-12 11:23:49 +0700

    update

---
+++ client/src/components/pages/Register/Register.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Register.css";
+function Register() {
+  return <h1>Register page</h1>;
+}
+
+export default Register;
