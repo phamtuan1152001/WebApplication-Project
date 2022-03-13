bc3e7e4d2521f56b50b319c17c1e43ecbf1ca68c
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2022-03-13 23:33:54 +0700

    Create Product Cart Component

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,27 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/pages/Home/Home";
 import ProductsPage from "./components/pages/Products/Products";
-//import ProductPage from "./components/pages/Products/Product"
 import ServicesPage from "./components/pages/Services";
 import ContactPage from "./components/pages/Contact";
 import LoginPage from "./components/pages/Login/Login";
 import RegisterPage from "./components/pages/Register/Register";
+import ProductPage from "./components/pages/Products/Product/Product";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/login" element={<LoginPage />}/>
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-        <Footer/>
-      </Router>s
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
