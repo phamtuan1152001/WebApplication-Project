fd6abee7c8c15985f458c5e2c529b884f471b051
gianamnguyen <gianamnguyen023@gmail.com>
2022-05-09 14:34:47 +0700

    update Contact

--- client/src/components/pages/Contact/Contact.js
+++ client/src/components/pages/Contact/Contact.js
@@ -5,12 +5,52 @@ import "./Contact.css";
 function Contact() {
   return (
     <>
-      <div>
-        <div className="">
+      <div class="contact-container">
+        <div class="left-col">
+          <img
+            src="/image/Contact-poster.jpg"
+            className="contact-img"
+            alt="Poster"
+          ></img>
+        </div>
+        <div class="right-col">
+          <h1>Contact us</h1>
+          <p></p>
 
+          <form id="contact-form" method="post">
+            <label for="name">Full name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your Full Name"
+              required
+            />
+            <label for="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your Email Address"
+              required
+            />
+            <label for="message">Message</label>
+            <textarea
+              rows="6"
+              placeholder="Your Message"
+              id="message"
+              name="message"
+              required
+            ></textarea>
+            <a href="\">
+              <button type="submit" id="submit" name="submit">
+                Submit
+              </button>
+            </a>
+          </form>
         </div>
       </div>
     </>
-  )
+  );
 }
 export default Contact;
