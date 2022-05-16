c40ece63bdbade3bdc183a529bf1dd5c789596cc
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2022-05-16 13:29:20 +0700

    fix Login/Register as Authorization

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+const API_URL = "http://localhost:5000/user/";
+const register = (
+  Firstname,
+  Lastname,
+  Address,
+  Phone,
+  email,
+  password,
+  confirmPassword
+) => {
+  return axios.post(API_URL + "signup", {
+    Firstname,
+    Lastname,
+    Address,
+    Phone,
+    email,
+    password,
+    confirmPassword,
+  });
+};
+const login = (email, password) => {
+  return axios
+    .post(API_URL + "signin", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.token) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      return response;
+      //   console.log(response.data.token);
+      //   console.log(response.data.user);
+      // console.log(response);
+    });
+};
+const logout = () => {
+  localStorage.removeItem("user");
+};
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+const AuthService = {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+};
+export default AuthService;
