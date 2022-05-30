935cd64dcae364e911cd6aa61b77acebf246637b
Song Tuan <75135018+phamtuan1152001@users.noreply.github.com>
2022-05-30 15:22:21 +0700

    Fix code and Adding UI

--- client/src/components/pages/Products/Products.js
+++ client/src/components/pages/Products/Products.js
@@ -42,6 +42,7 @@ function Products() {
           {/* <ProductSelection /> */}
           <div className="col">
             <button
+              style={{ width: "200px" }}
               className="btn btn-outline-secondary"
               onClick={() => setFilter(data)}
             >
@@ -50,6 +51,7 @@ function Products() {
           </div>
           <div className="col">
             <button
+              style={{ width: "200px" }}
               className="btn btn-outline-secondary"
               onClick={() => filterProducts("men's clothing")}
             >
@@ -58,6 +60,7 @@ function Products() {
           </div>
           <div className="col">
             <button
+              style={{ width: "200px" }}
               className="btn btn-outline-secondary"
               onClick={() => filterProducts("women's clothing")}
             >
@@ -66,6 +69,7 @@ function Products() {
           </div>
           <div className="col">
             <button
+              style={{ width: "200px" }}
               className="btn btn-outline-secondary"
               onClick={() => filterProducts("jewelery")}
             >
@@ -74,6 +78,7 @@ function Products() {
           </div>
           <div className="col">
             <button
+              style={{ width: "200px" }}
               className="btn btn-outline-secondary"
               onClick={() => filterProducts("electronics")}
             >
--- client/src/components/pages/Services.js
+++ client/src/components/pages/Services.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Services() {
+  // const handleClick = () => {
+  //   toast("hello")
+  // }
   return (
-    <h1>Services page</h1>
+    <>
+      <h1>Services page</h1>
+      {/* <button onClick={handleClick}>Click me</button> */}
+    </>
   );
 }
 
